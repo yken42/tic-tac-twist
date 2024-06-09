829559bc5820b11b6450cbbfe45d4a495e58cc3f
Yair Ken <yairkennn@gmail.com>
2024-06-09 20:50:13 +0300

    changed api url for integration with server

--- client/src/hooks/SocketContext.jsx
+++ client/src/hooks/SocketContext.jsx
@@ -4,12 +4,14 @@ import io from 'socket.io-client';
 const SocketContext = createContext();
 
 export const useSocket = () => useContext(SocketContext);
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export const SocketProvider = ({ children }) => {
+    
     const [socket, setSocket] = useState(null);
 
     useEffect(() => {
-        const newSocket = io.connect('http://localhost:3000');
+        const newSocket = io.connect(apiUrl);
         setSocket(newSocket);
 
         return () => {
